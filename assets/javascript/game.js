0d354fa4cac9b7a81725cd0e3bf4f95384f7861e
kevdsteele <48927209+kevdsteele@users.noreply.github.com>
2019-06-07 15:19:05 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,7 @@
-var questions = [ {Question: "", choices: ["","", "", ""], Answer: ""} , {Question: "", choices: ["","", "", ""], Answer: ""},{Question: "", choices: ["","", "", ""], Answer: ""},{Question: "", choices: ["","", "", ""], Answer: ""}]
+var questions = [ {Question: "Which MCU movie featured Spider-Man’s first appearance?", choices: ["Spider-Man: Homecoming","Avengers: Age of Ultron", "Iron Man 3", "Captain America: Civil War"], Answer: "Captain America: Civil War", GIFY:"https://media.giphy.com/media/KnjBhO9j4DVG8/giphy.gif"} , 
+                 {Question: "Who played the Hulk before Mark Ruffalo?", choices: ["Liam Hemsworth","Gary Oldman", "Edward Norton", "Will Smith"], Answer: "Edward Norton", GIFY: "https://media.giphy.com/media/aS8ypUweGOXMA/giphy.gif"},
+                 {Question: "What is the name of Thor’s hammer?", choices: ["Balder","Vanir", "Aesir", "Mjolnir"], Answer: "Mjolnir", GIFY: "https://media.giphy.com/media/xUPGGdn5TaL8Mfpwwo/giphy.gif"},
+                 {Question: "Where did Scott Lang work before becoming Ant-Man?", choices: ["Jamba Juice","Chipotle", "McDonald’s", "Baskin Robbins"], Answer: "Baskin Robbins", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"}]
 
 
 

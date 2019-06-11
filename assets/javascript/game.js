bad1fae832efb38d13ca6f0c0b322a60905d7c16
kevdsteele <48927209+kevdsteele@users.noreply.github.com>
2019-06-11 16:14:16 -0400

    Update game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,7 +6,18 @@ $(document).ready(function() {
 var questions = [ {Question: "Which MCU movie featured Spider-Man’s first appearance?", choices: ["Spider-Man: Homecoming","Avengers: Age of Ultron", "Iron Man 3", "Captain America: Civil War"], Answer: "Captain America: Civil War", GIFY:"https://media.giphy.com/media/KnjBhO9j4DVG8/giphy.gif"} , 
                  {Question: "Who played the Hulk before Mark Ruffalo?", choices: ["Liam Hemsworth","Gary Oldman", "Edward Norton", "Will Smith"], Answer: "Edward Norton", GIFY: "https://media.giphy.com/media/aS8ypUweGOXMA/giphy.gif"},
                  {Question: "What is the name of Thor’s hammer?", choices: ["Balder","Vanir", "Aesir", "Mjolnir"], Answer: "Mjolnir", GIFY: "https://media.giphy.com/media/xUPGGdn5TaL8Mfpwwo/giphy.gif"},
-                 {Question: "Where did Scott Lang work before becoming Ant-Man?", choices: ["Jamba Juice","Chipotle", "McDonald’s", "Baskin Robbins"], Answer: "Baskin Robbins", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"}]
+                 {Question: "Where did Scott Lang work before becoming Ant-Man?", choices: ["Jamba Juice","Chipotle", "McDonald’s", "Baskin Robbins"], Answer: "Baskin Robbins", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "What does Stark Industries manufacture in Iron Man 1?", choices: ["Weapons","Buildings", "Pharmaceuticals", "Chemicals"], Answer: "Weapons", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Which war did Captain America fight in?", choices: ["World War I","World War II", "Iraq War", "Vietnam War"], Answer: "World War II", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "What is Peter Quill’s alter ego?", choices: ["Star-Lord","Star-Dude", "Star-Man", "Star-Killer"], Answer: "Star-Lord", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "What legislation requires superhumans to be governed by law?", choices: ["Paris accord","Crimea accord", "Latveria accord", "Sokovia accord"], Answer: "Sokovia accord", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "What Nazi organization infiltrated SHIELD?", choices: ["EMPUSA","HYDRA", "CHIMERA", "SPHINX"], Answer: "HYDRA", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Who is Thor’s father?", choices: ["Loki","Hela", "Korg", "Odin"], Answer: "Odin", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Who is the main villain in Spider-Man: Homecoming?", choices: ["Venom","Mysterio", "Vulture", "Kraven"], Answer: "Vulture", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Which of these movies came first?", choices: ["Captain America: Civil War","Ant-Man", "Guardians of the Galaxy Vol. 2", "Doctor Strange"], Answer: "Ant-Man", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Who has the Infinity Gauntlet?", choices: ["Helmut Zemo","The Mandarin", "Thanos", "Ulysses Klaw"], Answer: "Thanos", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Where is Black Panther from?", choices: ["Nakia","T'Chaka", "Wakanda", "T'Challa"], Answer: "Wakanda", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"},
+                {Question: "Who plays Nick Fury?", choices: ["Morgan Freeman","Samuel L. Jackson", "Laurence Fishburne", "Denzel Washington"], Answer: "Samuel L. Jackson", GIF:"https://media.giphy.com/media/VIybTqiAz8E5W/giphy.gif"}]
 
 var answers = [];
 
